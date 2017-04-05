1170688a79a5bbebbb71690d68852972f04de3c1
JonjoyFang <1275841634@qq.com>
2017-04-05 17:48:12 +0800

    1

---
+++ apple/js/common.js
@@ -0,0 +1,9 @@
+$(function(){
+    // $('.icon_color_white').hover(function(){
+    //     $(this).css('color','rgba(255,255,255,0.8)');
+    // });
+    $('.imgs img').click(function(){
+        $('.imgs img').css('border-bottom','0');
+        $(this).css('border-bottom','1px solid #333');
+    })
+});
